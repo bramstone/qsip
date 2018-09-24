167c62943871b3dca1aa90742e84b9a200d4b683
Bram W Stone <stone.bram@gmail.com>
2018-09-24 10:02:38 -0700

    Updated missing data handling

--- R/calc_wad.R
+++ R/calc_wad.R
@@ -3,8 +3,8 @@
 #' Calculates weighted average densities for each microbial taxa in each sample replicate
 #'
 #' @param data Data as a \code{phyloseq} object
-#' @param ... Arguments to specify \code{NA} handling in the weighted average density calculation.
-#'   By default, \code{na.rm=FALSE}.
+# @param ... Arguments to specify \code{NA} handling in the weighted average density calculation.
+#   By default, \code{na.rm=FALSE}.
 #'
 #' @details Specifying \code{na.rm=TRUE} will allow \code{calc_wad} to calculate weighted average density values from samples
 #'   that have one or more fractions without a valid density value. The default setting, \code{na.rm=FALSE}, returns values
@@ -22,21 +22,23 @@
 #'
 #' @export
 
-calc_wad <- function(data, ...) {
+calc_wad <- function(data) {
   if(is(data)[1]!='phylosip') stop('Must provide phylosip object')
   if(length(data@qsip@rep_id)==0) stop('Must specify replicate IDs')
   rep_id <- data@sam_data[[data@qsip@rep_id]] # replicate IDs
   dv <- data@sam_data[[data@qsip@density]] # density values
   # transform sequencing abundances to 16S copy numbers
+  # returns matrix with taxa as columns, samples as rows
   ft <- copy_no(data)
   # split feature table by replicate ID
+  # the split matrix will be roughly 2x the size of the phylosip object
   dv <- split(dv, rep_id)
   ft <- split(ft, rep_id)
   ft <- base::lapply(ft, matrix,
                byrow=FALSE,
                ncol=phyloseq::ntaxa(data))
-  # next calculate WAD using lapply and combine
-  ft <- base::Map(function(y, x) apply(y, 2, wad, x, ...), ft, dv)
+  # next calculate WAD using Map (mapply) and apply by columns
+  ft <- base::Map(function(y, x) apply(y, 2, wad, x, na.rm=TRUE), ft, dv)
   # combine format based on whether taxa were rows or not
   if(phyloseq::taxa_are_rows(data)) {
     ft <- do.call(cbind, ft)
@@ -48,7 +50,7 @@ calc_wad <- function(data, ...) {
     colnames(ft) <- phyloseq::taxa_names(data)
   }
   # convert to S4 Matrix which is more memory efficient
-  ft <- Matrix::Matrix(ft)
+  # ft <- Matrix::Matrix(ft)
   # add wad values to data slot of qSIP portion of object
   if(!is.null(data@qsip@.Data$wad)) warning('Overwriting existing weighted average density values')
   data@qsip@.Data$wad <- ft
@@ -68,8 +70,8 @@ calc_wad <- function(data, ...) {
 #     output: weighted-average of the x-values (single value)
 #     Written by Ben Koch & Natasja van Gestel
 
-wad <- function(y, x, ...){
-  wad <- sum(x * (y / sum(y, na.rm=T)), ...)
+wad <- function(y, x, na.rm=TRUE){
+  wad <- sum(x * (y / sum(y, na.rm=TRUE)), na.rm=na.rm)
   return(wad)
 }
 
@@ -79,7 +81,7 @@ copy_no <- function(data) {
   ft <- as(data@otu_table, 'matrix')
   # make relative abundances with taxa as columns and samples as rows
   if(phyloseq::taxa_are_rows(data)) ft <- t(ft)
-  ft <- ft / base::colSums(ft, na.rm=T)
+  ft <- ft / base::colSums(ft, na.rm=TRUE)
   # multiply by total 16S copy number per sample
   ft <- ft * data@sam_data[[data@qsip@abund]]
   return(ft)
