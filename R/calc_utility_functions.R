5fd16d24cc02761533c4eedbf73af69310e24e61
Bram W Stone <stone.bram@gmail.com>
2018-09-25 11:46:42 -0700

    Generated utility functions to minimize calc_wad code

---
+++ R/calc_utility_functions.R
@@ -0,0 +1,50 @@
+# Collection of functions to make calculation function code neater
+
+# Calculation of 16S copies per taxon from relative abundances and 16S copy numbers
+# Note, will return a feature table with taxa as columns
+copy_no <- function(data) {
+  ft <- as(data@otu_table, 'matrix')
+  # make relative abundances with taxa as columns and samples as rows
+  if(phyloseq::taxa_are_rows(data)) ft <- t(ft)
+  ft <- ft / base::colSums(ft, na.rm=TRUE)
+  # multiply by total 16S copy number per sample
+  ft <- ft * data@sam_data[[data@qsip@abund]]
+  return(ft)
+}
+
+# Function used to split qSIP data into list of sub-matrices
+split_data <- function(data, new_data, grouping) {
+  grouping <- data@sam_data[[grouping]]
+  new_data <- split(new_data, grouping)
+  new_data <- base::lapply(new_data, matrix,
+                     byrow=FALSE,
+                     ncol=phyloseq::ntaxa(data))
+  return(new_data)
+}
+
+# Function used to handle adding new data to phylosip object .Data slot
+collate_results <- function(data, new_data, metric) {
+  # combine format based on whether taxa were rows or not
+  if(phyloseq::taxa_are_rows(data)) {
+    new_data <- do.call(cbind, new_data)
+  } else new_data <- do.call(rbind, new_data)
+  # add feature names back in (replicate names automatically utilized from split)
+  if(is.null(rownames(new_data))) {
+    rownames(new_data) <- phyloseq::taxa_names(data)
+  } else {
+    colnames(new_data) <- phyloseq::taxa_names(data)
+  }
+  # convert to S4 Matrix which is more memory efficient
+  new_data <- Matrix::Matrix(new_data)
+  # add wad values to data slot of qSIP portion of object
+  if(any(attributes(data@qsip)$names %in% metric)) { # if wad alreay exists, replace
+    warning('Overwriting existing ', metric, ' values', call.=FALSE)
+    replace_num <- which(attributes(data@qsip)$names %in% metric)
+    data@qsip@.Data[[replace_num]] <- new_data
+  }
+  else { # else append it to the list and update the list names
+    data@qsip@.Data[[length( data@qsip@.Data) + 1]] <- new_data
+    attributes(data@qsip)$names <- c(attributes(data@qsip)$names, metric)
+  }
+  return(data)
+}
--- R/calc_wad.R
+++ R/calc_wad.R
@@ -3,8 +3,6 @@
 #' Calculates weighted average densities for each microbial taxa in each sample replicate
 #'
 #' @param data Data as a \code{phyloseq} object
-# @param ... Arguments to specify \code{NA} handling in the weighted average density calculation.
-#   By default, \code{na.rm=FALSE}.
 #'
 #' @details Specifying \code{na.rm=TRUE} will allow \code{calc_wad} to calculate weighted average density values from samples
 #'   that have one or more fractions without a valid density value. The default setting, \code{na.rm=FALSE}, returns values
@@ -24,45 +22,21 @@
 
 calc_wad <- function(data) {
   if(is(data)[1]!='phylosip') stop('Must provide phylosip object')
-  if(length(data@qsip@rep_id)==0) stop('Must specify replicate IDs')
-  rep_id <- data@sam_data[[data@qsip@rep_id]] # replicate IDs
-  dv <- data@sam_data[[data@qsip@density]] # density values
+  if(length(data@qsip@rep_id)==0) stop('Must specify replicate IDs with rep_id')
   # transform sequencing abundances to 16S copy numbers
   # returns matrix with taxa as columns, samples as rows
   ft <- copy_no(data)
-  # split feature table by replicate ID
-  # the split matrix will be roughly 2x the size of the phylosip object
-  dv <- split(dv, rep_id)
-  ft <- split(ft, rep_id)
-  ft <- base::lapply(ft, matrix,
-               byrow=FALSE,
-               ncol=phyloseq::ntaxa(data))
-  # next calculate WAD using Map (mapply) and apply by columns
+  # manipulate data matrix and calculate
+  ft <- split_data(data, ft, data@qsip@rep_id) # split by replicate IDs
+  dv <- split(data@sam_data[[data@qsip@density]],
+              data@sam_data[[data@qsip@rep_id]]) # split densities by replicate IDs
   ft <- base::Map(function(y, x) apply(y, 2, wad, x, na.rm=TRUE), ft, dv)
-  # combine format based on whether taxa were rows or not
-  if(phyloseq::taxa_are_rows(data)) {
-    ft <- do.call(cbind, ft)
-  } else ft <- do.call(rbind.ft)
-  # add feature names back in (replicate names automatically utilized from split)
-  if(is.null(rownames(ft))) {
-    rownames(ft) <- phyloseq::taxa_names(data)
-  } else {
-    colnames(ft) <- phyloseq::taxa_names(data)
-  }
-  # convert to S4 Matrix which is more memory efficient
-   ft <- Matrix::Matrix(ft)
-  # add wad values to data slot of qSIP portion of object
-  if(any(attributes(data@qsip)$names %in% 'wad')) { # if wad alreay exists, replace
-    warning('Overwriting existing weighted average density values', call.=FALSE)
-    replace_num <- which(attributes(data@qsip)$names %in% 'wad')
-    data@qsip@.Data[[replace_num]] <- ft
-  }
-  else { # else append it to the list and update the list names
-    data@qsip@.Data[[length( data@qsip@.Data) + 1]] <- ft
-    attributes(data@qsip)$names <- c(attributes(data@qsip)$names, 'wad')
-  }
+  # organize and add new data as S4 matrix
+  data <- collate_results(data, ft, 'wad')
   return(data)
 }
+
+# Utility functions to be used to do actual calculations
 
 # Given vectors of x values and y values, calculate the weighted-average of the x-values (e.g., the weighted average density (WAD))
 # With na.rm=F, any samples with NA density values will be returned with all taxa's WAD values as NA. (the default action)
@@ -77,19 +51,7 @@ calc_wad <- function(data) {
 #     output: weighted-average of the x-values (single value)
 #     Written by Ben Koch & Natasja van Gestel
 
-wad <- function(y, x, na.rm=TRUE){
+wad <- function(y, x, na.rm=TRUE) {
   wad <- sum(x * (y / sum(y, na.rm=TRUE)), na.rm=na.rm)
   return(wad)
-}
-
-# Calculation of 16S copies per taxon from relative abundances and 16S copy numbers
-# Note, will return a feature table with taxa as columns
-copy_no <- function(data) {
-  ft <- as(data@otu_table, 'matrix')
-  # make relative abundances with taxa as columns and samples as rows
-  if(phyloseq::taxa_are_rows(data)) ft <- t(ft)
-  ft <- ft / base::colSums(ft, na.rm=TRUE)
-  # multiply by total 16S copy number per sample
-  ft <- ft * data@sam_data[[data@qsip@abund]]
-  return(ft)
 }
