918cd651753020167db711b3c90f6f43916a4483
Bram W Stone <stone.bram@gmail.com>
2018-09-27 10:23:48 -0700

    Added APE calculations if percent == T

--- R/calc_excess.R
+++ R/calc_excess.R
@@ -20,13 +20,16 @@
 #'
 #'  # Calculate atom fraction excess
 #'
+#'  # compare
+#'  all.equal(aef*100, ape)
+#'
 #' @export
 
 calc_excess <- function(data, percent=FALSE) {
   if(is(data)[1]!='phylosip') stop('Must provide phylosip object')
   # if delta-WAD values don't exist, calculate those first
   # this will also handle rep_id validity (through calc_wad) and rep_group/iso_trt validity (through calc_d_wad)
-  if(is.null(data@qsip[['mw_label']] || is.null(data@qsip[['mw_light']])) data <- calc_mw(data)
+  if(is.null(data@qsip[['mw_label']]) || is.null(data@qsip[['mw_light']])) data <- calc_mw(data)
   # extract MW-labeled / MW-light and convert to S3 matrices
   mw_lab <- data@qsip[['mw_label']]
   mw_lab <- as(mw_lab, 'matrix')
@@ -34,14 +37,19 @@ calc_excess <- function(data, percent=FALSE) {
   mw_l <- as(mw_l, 'matrix)')
   if(phyloseq::taxa_are_rows(data)) mw_lab <- t(mw_lab); mw_l <- t(mw_l)
   # calculate mol. weight heavy max (i.e., what is maximum possible labeling)
-  # 13C labeling must account for GC content because a GC bp has 1 less C atom than an AT bp
-  if(iso=='18O') adjust <- 12.07747 + mw_l; nat_abund <- 0.002000429
-  else if(iso=='13C') adjust <- (-0.4987282 * gc) + 9.974564; nat_abund <- 0.01111233
+  if(iso=='18O') {
+    adjust <- 12.07747 + mw_l
+    nat_abund <- 0.002000429
+  }
+  else if(iso=='13C') {
+    adjust <- (-0.4987282 * gc) + 9.974564
+    nat_abund <- 0.01111233
+  }
   mw_max <- adjust + mw_l
   # calculate atom excess
   excess <- ((mw_lab - mw_l)/(mw_max - mw_l)) * (1- nat_abund)
   # organize and add new data as S4 matrices
-  if(percent) #...
+  if(percent) excess <- excess * 100
   data <- collate_results(data, excess, 'atom_excess')
   return(data)
 }
