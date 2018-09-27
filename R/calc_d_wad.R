ef0cf59c55316cb66fa3dc6fed6563991c634b86
Bram W Stone <stone.bram@gmail.com>
2018-09-27 09:59:25 -0700

    Returns WAD values of light treatments automatically

--- R/calc_d_wad.R
+++ R/calc_d_wad.R
@@ -3,16 +3,14 @@
 #' Calculates differences in weighted average densities across replicates due to isotope incorporation
 #'
 #' @param data Data as a \code{phyloseq} object
-#' @param return_wad_light Logical value indicating whether or not to also return mean weighted average densities of light
-#'   (no isotope) treatments, which are necessary for the calculation of molecular weights with and without isotopes.
 #'
 #' @details Some details about proper isotope control-treatment factoring. If weighted average densities have not been calculated
 #'   beforehand, \code{calc_d_wad} will compute those first.
 #'
-#' @return \code{calc_d_wad} adds an S4 Matrix class (which more efficiently stores sparse matrix data) to the \code{.Data} slot within
-#'   the \code{qsip} slot of the object of differences in weighted average density values for each taxon at each group of replicates. The row and column
-#'   specifications will mirror those of the \code{phylosip}'s \code{\link{otu_table}}, meaning if taxa are listed on the table rows,
-#'   they will in the resulting S4 Matrix class.
+#' @return \code{calc_d_wad} adds two S4 Matrix objects to the \code{data@@qsip@@.Data} slot, one for differences
+#'   in weighted average density, and the other for weighted average density values of light treatments only (to be used in
+#'   future calculations). The row and column specifications will mirror those of the \code{phylosip}'s \code{\link{otu_table}},
+#'   meaning if taxa are listed on the table rows, they will in the resulting S4 Matrix objects
 #'
 #' @seealso \code{\link{calc_wad}}
 #'
@@ -23,7 +21,7 @@
 #'
 #' @export
 
-calc_d_wad <- function(data, return_wad_light=TRUE) {
+calc_d_wad <- function(data) {
   if(is(data)[1]!='phylosip') stop('Must provide phylosip object')
   # if WAD values don't exist, calculate those first, this will also handle rep_id validity
   if(is.null(data@qsip[['wad']])) data <- calc_wad(data)
@@ -46,7 +44,6 @@ calc_d_wad <- function(data, return_wad_light=TRUE) {
   iso_group <- iso_group[keep_rows,]
   ft <- split_data(data, ft, iso_group$interaction, grouping_w_phylosip=F)
   # calculate average WAD per taxa for each replicate group
-  # ft <- base::lapply(ft, function(x) base::apply(x, 2, mean, na.rm=T))
   ft <- base::lapply(ft, colmeans, na.rm=T)
   # create a new list to add results of mean WAD difference into
   d_ft <- as.list(rep(0, nlevels(iso_group$grouping)))
@@ -56,6 +53,7 @@ calc_d_wad <- function(data, return_wad_light=TRUE) {
                  ncol=phyloseq::ntaxa(data))
   names(d_ft) <- levels(iso_group$grouping)
   # For each repliate group: identify which elements of ft are light and which are heavy, then get difference
+  # ALTERNATIVE, CALCULATE AVERAGE WAD_LIGHT VALUES EXPERIMENT-WIDE
   iso_group2 <- unique(iso_group[,!names(iso_group) %in% 'replicate']) # only get unique elements to match levels in ft
   for(i in 1:length(d_ft)) {
     which_light <- which(as.numeric(iso_group2$grouping)==i &
@@ -69,9 +67,7 @@ calc_d_wad <- function(data, return_wad_light=TRUE) {
   # organize and add new data as S4 matrix
   data <- collate_results(data, d_ft, 'd_wad')
   # return weighted average densities of light calcs only
-  if(return_wad_light) {
-    wl <- ft[[as.numeric(iso_group2$iso)==1]]
-    data <- collate_results(data, wl, 'wad_light')
-  }
+  wl <- ft[[as.numeric(iso_group2$iso)==1]]
+  data <- collate_results(data, wl, 'wad_light')
   return(data)
 }
