a3ebcb64c96ff8357edb5e0ac56f5836ca7444f3
Bram W Stone <stone.bram@gmail.com>
2018-10-09 13:47:51 -0700

    Added support for filtering

--- R/calc_excess.R
+++ R/calc_excess.R
@@ -9,6 +9,8 @@
 #'   The default is blank indicating that no confidence intervals will be calculated.
 #' @param ci Numeric value from 0 to 1 indicating the width of the confidence interval for bootsrapped atom excess values.
 #' @param iters Number of (subsampling) iterations to conduct to calculate confidence intervals. Default is \code{999}.
+#' @param filter Logical vector specifying whether or not to filter taxa from the weighted average density calculation.
+#'   This will require \code{data} to have a filter applied with \code{\link{filter_qsip}}.
 #'
 #' @details Some details about proper isotope control-treatment factoring. If weighted average densities or the change in weighted average densities
 #'   have not been calculated beforehand, \code{calc_mw} will compute those first.
@@ -30,10 +32,10 @@
 #'
 #' @export
 
-calc_excess <- function(data, percent=FALSE, ci_method='', ci=.95, iters=999) {
+calc_excess <- function(data, percent=FALSE, ci_method=c('', 'bootstrap', 'bayesian'), ci=.95, iters=999, filter=FALSE) {
   if(is(data)[1]!='phylosip') stop('Must provide phylosip object')
+  if(missing(ci_method)) ci_method <- ''
   ci_method <- match.arg(tolower(ci_method), c('', 'bootstrap', 'bayesian'))
-  # maybe filter qSIP here
   #
   # -------------------------------------------------------------
   # no CI and resampling
@@ -41,7 +43,7 @@ calc_excess <- function(data, percent=FALSE, ci_method='', ci=.95, iters=999) {
   if(ci_method=='') {
     # if MW values don't exist, calculate those first
     # this will also handle rep_id validity (through calc_wad) and rep_group/iso_trt validity (through calc_d_wad)
-    if(is.null(data@qsip[['mw_label']]) || is.null(data@qsip[['mw_light']])) data <- calc_mw(data)
+    if(is.null(data@qsip[['mw_label']]) || is.null(data@qsip[['mw_light']])) data <- calc_mw(data, filter=filter)
     # extract MW-labeled and convert to S3 matrix with taxa as ROWS (opposite all other calcs)
     mw_lab <- data@qsip[['mw_label']]
     mw_lab <- as(mw_lab, 'matrix')
@@ -70,8 +72,8 @@ calc_excess <- function(data, percent=FALSE, ci_method='', ci=.95, iters=999) {
     #
   } else if(ci_method=='bootstrap') {
     # Calc WADs
-    if(is.null(data@qsip[['wad']])) data <- calc_wad(data)
-    ft <- as(ft, 'matrix')
+    if(is.null(data@qsip[['wad']])) data <- calc_wad(data, filter=filter)
+    ft <- as(data@qsip[['wad']], 'matrix')
     if(phyloseq::taxa_are_rows(data)) ft <- t(ft)
     iso_group <- iso_grouping(data, data@qsip@iso_trt, data@qsip@rep_id, data@qsip@rep_group)
     # keep only valid rows
@@ -95,20 +97,21 @@ calc_excess <- function(data, percent=FALSE, ci_method='', ci=.95, iters=999) {
                               subsample,
                               nrow=subsample_n,
                               byrow=FALSE)
-    # collect output in S4 class matrix (each column is an atom excess matrix from that iterations subsampling)
+    # collect output in matrix (each column is an atom excess matrix from that iterations subsampling)
+    # Note: this matrix will be very large if you don't filter taxa out first
     boot_collect <- matrix(0,
-                           nrow=ncol(ft) * levels(data@sam_data[[data@qsip@rep_group]]),
+                           nrow=ncol(ft) * nrow(ft),
                            ncol=iters)
     boot_rnames <- expand.grid(rownames(excess_i),
                                colnames(excess_i),
                                stringsAsFactors=FALSE)
     rownames(boot_collect) <- interaction(boot_rnames[,1], boot_rnames[,2], sep=':')
-    boot_collect <- Matrix::Matrix(boot_collect, sparse=TRUE)
     rm(boot_rnames)
     for(i in 1:iters) {
-      # subsample WAD values
+      # subsample WAD values, calc diff_WAD and molecular weights
       subsample_i <- lapply(subsample, function(x) x[,i])
       ft_i <- mapply(function(x, y) x[y,], ft, subsample_i)
+      ft_i <- do.call(rbind, ft_i)
       data <- collate_results(data, ft_i, 'wad', sparse=TRUE)
       data <- calc_mw(data)
       mw_lab <- data@qsip[['mw_label']]
@@ -116,7 +119,7 @@ calc_excess <- function(data, percent=FALSE, ci_method='', ci=.95, iters=999) {
       mw_l <- data@qsip[['mw_light']]
       if(!is.null(dim(mw_l))) mw_l <- as(mw_l, 'matrix')   # if mw_l is matrix, convert to S3 matrix
       if(!phyloseq::taxa_are_rows(data)) mw_lab <- t(mw_lab)
-      # calculate mol. weight heavy max (i.e., what is maximum possible labeling)
+      # calculate atom excess for this subsampling iteration
       if(data@qsip@iso=='18O') {
         adjust <- 12.07747 + mw_l
         nat_abund <- 0.002000429
@@ -126,7 +129,7 @@ calc_excess <- function(data, percent=FALSE, ci_method='', ci=.95, iters=999) {
         nat_abund <- 0.01111233
       }
       mw_max <- adjust + mw_l
-      # calculate atom excess
+      # atom excess
       excess <- ((mw_lab - mw_l)/(mw_max - mw_l)) * (1 - nat_abund)
       # organize and add data as single column in bootstrap output matrix
       if(percent) excess <- excess * 100
@@ -135,13 +138,31 @@ calc_excess <- function(data, percent=FALSE, ci_method='', ci=.95, iters=999) {
     }
     rm(ft_i, subsample, subsample_n)
     # summarize across iterations (lower CI, median, upper CI)
-    # reconstruct matrices and output
+    summaries <- t(apply(boot_collect, 1,
+                         quantile,
+                         c((1 - ci)/2, .5, (1 - ci)/2 + ci)))
+    ci_l <- summaries[,1]
+    med <- summaries[,2]
+    ci_u <- summaries[,3]
+    rm(boot_collect, summaries)
+    # reconstruct matrices with taxa as columns and output
+    ci_l <- matrix(ci_l, ncol=phyloseq::ntaxa(data), byrow=TRUE)
+    med <- matrix(med, ncol=phyloseq::ntaxa(data), byrow=TRUE)
+    ci_u <- matrix(ci_u, ncol=phyloseq::ntaxa(data), byrow=TRUE)
+    rownames(ci_l) <- rownames(med) <- rownames(ci_u) <- rownames(excess)
+    ci_l_name <- paste0('atom_excess_',ci*100,'_l')
+    ci_u_name <- paste0('atom_excess_',ci*100,'_u')
+    data <- collate_results(data, ci_l, ci_l_name, sparse=TRUE)
+    data <- collate_results(data, med, 'atom_excess', sparse=TRUE)
+    data <- collate_results(data, ci_l, ci_u_name, sparse=TRUE)
     return(data)
     #
     # -------------------------------------------------------------
     # CI values obtained through Bayesian analysis
     #
   } else if(ci_method=='bayesian') { # method for bayesian analysis
+    print('No Bayesian method yet, returning data unaltered')
+    return(data)
     # code here.....
   }
 }
