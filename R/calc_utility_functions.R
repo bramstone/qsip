d1db12fa5a96e82bd44d61299ce948bb37e6f8ca
Bram W Stone <stone.bram@gmail.com>
2018-10-16 15:38:06 -0700

    Created summarize_ci() function to calc CIs and organize output

--- R/calc_utility_functions.R
+++ R/calc_utility_functions.R
@@ -95,3 +95,22 @@ collate_results <- function(data, new_data, tax_names=NULL, metric, ...) {
   }
   return(data)
 }
+
+# function that generates confidence intervals for a metric (lower CI, median, upper CI)
+# also handles row and column names. Returns a list
+summrize_ci <- function(bootstraps, ci, list_names=c('ci_l', 'med', 'ci_u'), tax_names, grouping) {
+  summaries <- t(apply(bootstraps, 1,
+                       quantile,
+                       c((1 - ci)/2, .5, (1 - ci)/2 + ci),
+                       na.rm=TRUE))
+  ci_l <- summaries[,1]
+  med <- summaries[,2]
+  ci_u <- summaries[,3]
+  colnames(ci_l) <- colnames(med) <- colnames(ci_u) <- tax_names
+  if(isTRUE(all.equal(grouping[,1], grouping$grouping))) {
+    rownames(ci_l) <- rownames(med) <- rownames(ci_u) <- unique(grouping$grouping[as.numeric(grouping$grouping)==2])
+  } else rownames(ci_l) <- rownames(med) <- rownames(ci_u) <- levels(grouping$grouping)
+  output <- list(ci_l, med, ci_u)
+  names(output) <- list_names
+  return(output)
+}
