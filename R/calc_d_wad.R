4e57858c49b6928ea712ed57b88cfc0ecd320998
Bram W Stone <stone.bram@gmail.com>
2018-12-18 09:52:04 -0700

    Removed separate_label argument

--- R/calc_d_wad.R
+++ R/calc_d_wad.R
@@ -10,9 +10,6 @@
 #' @param separate_light Logical value indicating whether or not WAD-light scores should be averaged across all replicate groups or not.
 #'   If \code{FALSE}, unlabeled WAD scores across all replicate groups will be averaged, creating a single molecular weight score per taxon
 #'   representing it's genetic molecular weight in the absence of isotope addition.
-#' @param separate_label Logical value indicating whether or not WAD-label scores should be averaged across all replicate groups or not.
-#'   If \code{FALSE}, labeled WAD scores across all replicate groups will be averaged, creating a single molecular weight score per taxon
-#'   representing it's genetic molecular weight as a result of isotope addition. The default is \code{TRUE}.
 #' @param correction Logical value indicating whether or not to apply tube-level correction to labeled WAD values.
 #' @param offset_taxa Value from 0 to 1 indicating the percentage of the taxa to utilize for calculating offset correction values.
 #'   Taxa are ordered by lowest difference in WAD values.
@@ -41,7 +38,7 @@
 #'
 #' @export
 
-calc_d_wad <- function(data, filter=FALSE, return_diffs=FALSE, correction=FALSE, offset_taxa=0.1, separate_light=FALSE, separate_label=TRUE) {
+calc_d_wad <- function(data, filter=FALSE, return_diffs=FALSE, correction=FALSE, offset_taxa=0.1, separate_light=FALSE) {
   if(is(data)[1]!='phylosip') stop('Must provide phylosip object')
   # if WAD values don't exist, calculate those first, this will also handle rep_id validity
   if(is.null(data@qsip[['wad']])) data <- calc_wad(data, filter=filter)
