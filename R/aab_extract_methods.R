9d86e7c2493b65e1d054b02a32fd6326f19c2572
Bram W Stone <stone.bram@gmail.com>
2019-02-25 15:19:20 -0700

    Changed method to automatically return S3 matrix (or vector) with 0s converted to NA

--- R/aab_extract_methods.R
+++ R/aab_extract_methods.R
@@ -6,5 +6,9 @@ setMethod("[[", "qsip", function(x, i, ...) {
   match_name <- which(attributes(x)$names==i)
   if(length(match_name)==0) return(NULL)
   y <- as.list(x@.Data)[[match_name]]
-  return(as(y, 'matrix'))
+  if(!is.null(dim(y))) {
+    y <- (as(y, 'matrix'))
+  }
+  y[0] <- NA
+  return(y)
 })
