e740cb61e95a957bfe18de624b49377b8befcbb9
Bram Stone <stone.bram@gmail.com>
2023-09-29 12:04:30 -0700

    Update calc_utility_functions.R

--- R/calc_utility_functions.R
+++ R/calc_utility_functions.R
@@ -2,8 +2,11 @@
 
 # function which prints sequence count and ASV count
 # used in filtering taxa function
-seq_check <- function(x) {
-  nums <- formatC(c(x[, sum(seq_abund)], x[, uniqueN(taxon_id)]), format = 'fg', width = 12, big.mark = ',')
+seq_summary <- function(x, reads = c(), tax_id = c()) {
+  if(is.null(reads) || is.null(tax_id)) stop("Must supply columns for sequence reads and taxon ID")
+  nums <- formatC(c(sum(x[[reads]], na.rm = TRUE), 
+                    uniqueN(x[[tax_id]])), 
+                  format = 'fg', width = 12, big.mark = ',')
   cat(nums[1], ' sequences\n', nums[2], ' ASVs', sep = ''); cat('\n')
 }
 
