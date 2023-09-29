214223c3f61f2584766eb3de5898b38d901f282d
Bram Stone <stone.bram@gmail.com>
2023-09-29 12:54:43 -0700

    Update documentation for freq_filter.R

--- R/freq_filter.R
+++ R/freq_filter.R
@@ -10,8 +10,12 @@
 #'
 #' @details There is no strong agreement among qSIP-users on the proper threshold to set for removing rare features.
 #'   Rare and infrequent taxa produce noise in the data, making it hard to discern quality.
-#'   The one guiding principle that there may be agreement on is that it’s best to set minimum filters at first -- to be as inclusive as possible – 
+#'   The one guiding principle that there may be agreement on is that it’s best to set minimum filters at first -- to be as inclusive as possible –- 
 #'   and intensify filters as needed to reduce noise.
+#'
+#'   One or more columns may be specified in the filter_target parameter, allowing for frequency filtering across treatment groups.
+#'   My recommendation in this case is to make sure that all non-labeled samples (those where isotopic composition is at natural abundance)
+#'   grouped together so that non-labeled buoyant density estimates may be made with as many occurrences as possible for each taxon.
 #'
 #'
 #' @return Returns filtered data table with taxa above specified frequency thresholds
